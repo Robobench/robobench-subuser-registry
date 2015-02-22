68fcea6ca39911f2082473499b90c17d4ae7c5ca
Jon Weisz <jon.weisz@gmail.com>
2015-02-22 13:02:47 -0500

    Added host descriptor to build script

---
+++ drone/docker-image/BuildImage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+scriptpath=$(readlink -f "$0")
+utilpath=$(dirname $scriptpath)/../../utils
+
+# Get the host descriptor
+bash $utilpath/get_host_descriptor.bash
+
+#create a subuser directory with the uuid
+rm -rf ./subuser
+mkdir -p ./subuser
+
+#the uuid is the first argument. Create the uuid file and make sure it is added in the build file
+echo $1 > ./subuser/uuid
